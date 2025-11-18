f2672a8ce7935879c91083f1d4fd46da14bd2590
MinhTan <tanbeo31102005@gmail.com>
2025-11-18 18:31:29 +0700

    thêm chức năng thêm giao dịch

---
+++ ExpenseManager.App/Repositories/TransactionRepository.cs
@@ -0,0 +1,23 @@
+﻿using ExpenseManager.App.Models.EF;
+using ExpenseManager.App.Models.Entities;
+using ExpenseManager.App.Repositories.Interfaces;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.App.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly ExpenseDbContext _context;
+
+        public TransactionRepository(ExpenseDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddTransactionAsync(Transaction transaction)
+        {
+            await _context.Transactions.AddAsync(transaction);
+            // Lưu ý: Chúng ta chưa gọi SaveChanges ở đây để xử lý Transaction (Database Transaction) bên Service
+        }
+    }
+}
---
+++ ExpenseManager.App/Services/ITransactionService.cs
@@ -0,0 +1,10 @@
+﻿using ExpenseManager.App.Models.Entities;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.App.Services.Interfaces
+{
+    public interface ITransactionService
+    {
+        Task AddTransactionAsync(Transaction transaction);
+    }
+}
